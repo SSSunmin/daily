c0ccd4b85939927c8abf3eac1b69e47018e85afb
sunmin <dlatjsals123@naver.com>
2025-06-03 01:41:10 +0900

    수정

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     cors: true,
     proxy: {
       '/api': {
-        target: 'http://3.39.239.224:8080',
+        target: 'http://ec2-3-39-239-224.ap-northeast-2.compute.amazonaws.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
